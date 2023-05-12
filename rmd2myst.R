9ac41495b8170ae97cdfa430bda6680d3209803d
Jannik <45204741+jhorzek@users.noreply.github.com>
2023-05-12 13:22:47 +0200

    Update rmd2myst.R
    Ich habe die Möglichkeit eingebaut, einen neue yaml-header zu übergeben und die Ersetzung der code-chunks flexibler gemacht

--- rmd2myst.R
+++ rmd2myst.R
@@ -1,12 +1,22 @@
 # Dieses R-Script übersetzt automatisch alle Rmd Dateien aus dem Ordner Aufgaben_rmd in Markdown Dateien, 
 # die für die Website verwendet werden können.
 
-
-rmd2myst <- function(x) {
+#' rmd2myst
+#' @param file_name Name der Rmd Datei. Diese muss sich im aktuellen working directory befinden
+#' @param myst_yaml optional: YAML header für die myst - Datei
+#' @return erstellt eine .md Datei im myst - Syle
+rmd2myst <- function(file_name, myst_yaml = NULL) {
+  # Einlesen der Rmd Datei
   save_as <- gsub("\\.Rmd", ".md", x)
   dat <- file(x)
   x <- readLines(dat)
-  close(dat) #warum?
+  close(dat)
+  
+  # Leere Zeilen zu Beginn der Datei entfernen, falls die Datei nicht
+  # mit dem yaml header beginnt.
+  for(i in 1:length(x))
+    if(x[i] != "") break;
+  x <- x[i:length(x)]
   
   # yaml entfernen
   if (grepl("---", x[1])) {
@@ -14,13 +24,40 @@ rmd2myst <- function(x) {
     x <- x[(yaml_end + 1L):length(x)]
   }
   
-  # neuer YAML Header -> Daten werden aus aufgabe_test Datei ausgelesen 
-  dat_meta <- readLines(con = "aufgabe_test.md", n = -1L)
-  meta_length <- grep("---", dat_meta)[2] - grep("---", dat_meta)[1] + 1L
-  metadata <- readLines(con = "aufgabe_test.md", n = meta_length)  # Klammer funktioniert nicht????                                                                                                                     )
-  x <- c(metadata, x)
+  # Neuer YAML Header. Wir definieren hier den neuen Standard header,
+  # falls keiner übergeben wurde
+  if(is.null(myst_yaml))
+    myst_yaml <- 
+"
+---
+jupytext:
+  formats: md:myst
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.11.5
+kernelspec:
+  display_name: R
+  language: R
+  name: ir
+---
+"
+ x <- c(myst_yaml,
+        x)
+
+  # Ersetzen der code-chunks
   
-  x <- gsub("```\\{r\\}","```\\{code-cell\\} r", x)
+  x <- gsub(pattern = "```\\{ *r[ \\sa-zA-Z0-9\\_]*\\}",
+            replacement = "```\\{code-cell\\} r", 
+            x = x)
+  
+  # Überprüfen, ob alle code-chunks ersetzt wurden:
+  if(any(grepl(pattern = "```\\{[\\s]*r", x = x))){
+    stop("Die folgenden code-chunk konnte nicht automatisch übersetzt werden: \n",
+         paste0(x[grepl(pattern = "```\\{[\\s]*r", x = x)], collapse = "\n")
+         )
+  }
  
   cat(x, file = save_as, sep = "\n")
 }
